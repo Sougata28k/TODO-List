1a9e5a4677974171f6fb70478e1ad1db0f9f690b
Sougata Karmakar <86069522+Sougata28k@users.noreply.github.com>
2023-09-12 13:22:43 +0530

    Update script.js
    added logic for showing the current date

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 // public/script.js
+
+    const currentDate = new Date();
+    
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth() + 1; 
+    const year = currentDate.getFullYear();
+
+    const formattedDate = `${month}.${day}.${year}`;
+
+    document.getElementById("current-date").textContent = `${formattedDate}`;
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const todoList = document.getElementById('todo-list');
     const taskInput = document.getElementById('task-input');
@@ -8,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const listItem = document.createElement('li');
         listItem.innerHTML = `
             <span class="task">${taskText}</span>
-            <button class="delete-button">Delete</button>
+            <button class="delete-button" style="background-color: #ff6347; color: #fff; border: none; padding: 5px 10px; cursor: pointer;border-radius:10px; box-shadow: 0 10px 10px rgba(0, 0, 0, 0.2);">Delete</button>
         `;
 
         const deleteButton = listItem.querySelector('.delete-button');
